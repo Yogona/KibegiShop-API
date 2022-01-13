eb66bde6dcb11f3d45b755a12058d86f0bbab099
Yogona <yonagodwin@gmail.com>
2022-01-13 19:04:30 +0300

    Added authorization to view user profile.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use \App\Models\User;
+use Illuminate\Support\Facades\Gate;
 
 class UserController extends Controller
 {
-    public function getUser($id){
-        $user = User::find($id);
+    public function getUser(Request $request, $id){
+        $searchUser = User::find($id);
+       
+        if($searchUser){
+            if(!Gate::forUser($request->user())->check('view-user', $searchUser)){
+                return response()->json([
+                    'status' => '403',
+                    'message' => 'You can not view this user.',
+                ]);
+            }
+        }
 
-        if(!$user){
+        if(!$searchUser){
             return response()->json([
                 'status' => '204',
                 'message' => 'User was not found.',
@@ -20,7 +30,7 @@ class UserController extends Controller
         return response()->json([
             'status' => '200',
             'message' => 'User was found successfully.',
-            'body' => $user,
+            'body' => $searchUser,
         ]);
     }
 }
